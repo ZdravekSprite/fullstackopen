f0399cbe614b91a13609e42468742c5891f53e94
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-09-28 17:34:06 +0200

    2.5 end [part2]

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,34 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name = { course.name } />
-      <Content parts = { course.parts } />
-      <Total parts = { course.parts } />
-    </div>
-  )
-}
-
-const Header = ({ name }) =>
-  <h2>{ name }</h2>
-
-const Content = ({ parts }) =>
-  <div>{ parts.map(part => <Part key={part.id} part={part} />)}</div>
-
-const Part = ({ part }) =>
-  <p>{ part.name } { part.exercises }</p>
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, p) => {
-    console.log('what is happening', s, p)
-    return s + p.exercises
-  }, 0)
-  return (
-    <p><b>total of { total } exercises</b></p>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
