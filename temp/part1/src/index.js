39006f7e455f55ef53b9104c480881c86f7433f9
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-09-28 10:59:02 +0200

    1c Page re-rendering [part1]

--- temp/part1/src/index.js
+++ temp/part1/src/index.js
@@ -1,33 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = ({ name, age }) => {
-//const Hello = (props) => {
-/*  const name = props.name
-  const age = props.age*/
-//  const { name, age } = props
-
-  const bornYear = () => new Date().getFullYear() - age
-
+const App = (props) => {
+  const { counter } = props
   return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const name = 'Arto Hellas'
-  const age = 35
+let counter = 1
 
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
+const refresh = () => {
+  ReactDOM.render(<App counter={counter} />, 
+  document.getElementById('root'))
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
