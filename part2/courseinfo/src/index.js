4b5c6482e1fd4c88efec9a3360ad501ef194d490
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-09-28 17:06:33 +0200

    2.2 step7 end [part2]

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -6,33 +6,23 @@ const Course = ({ course }) => {
     <div>
       <Header name = { course.name } />
       <Content parts = { course.parts } />
+      <Total parts = { course.parts } />
     </div>
   )
 }
 
-const Header = ({ name }) => {
-  return (
-    <h1>{ name }</h1>
-  )
-}
+const Header = ({ name }) =>
+  <h1>{ name }</h1>
 
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => 
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-}
+const Content = ({ parts }) =>
+  <div>{ parts.map(part => <Part key={part.id} part={part} />)}</div>
 
-const Part = ({ part }) => {
-  return (
-    <p>
-      { part.name } { part.exercises }
-    </p>    
-  )
-}
+const Part = ({ part }) =>
+  <p>{ part.name } { part.exercises }</p>
+
+const Total = ({ parts }) =>
+  <p><b>total of { parts.reduce((sum, parts) => sum + parts.exercises, 0) } exercises</b></p>
+
 const App = () => {
   const course = {
     id: 1,
@@ -52,6 +42,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
