8649e04da8769e36ca0da9fe9b8b4ef114d32126
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-09-28 11:14:44 +0200

    1c Event handling [part1]

--- temp/part1/src/index.js
+++ temp/part1/src/index.js
@@ -4,15 +4,16 @@ import ReactDOM from 'react-dom'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter)
-
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}> 
+        zero
+      </button>
+    </div>
   )
 }
 
