24f299a7a2911a01b6c9d5674cf16e9b91089b04
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-09-28 10:34:44 +0200

    java_script 2 [part1]

--- temp/part1/src/index.js
+++ temp/part1/src/index.js
@@ -22,13 +22,6 @@ const App = () => {
   console.log(first, second)  // 1, 2 is printed
   console.log(rest)          // [3, 4 ,5] is printed
 
-  const object1 = {
-    name: 'Arto Hellas',
-    age: 35,
-    education: 'PhD',
-  }
-  console.log(object1)
-
   const object2 = {
     name: 'Full Stack web application development',
     level: 'intermediate studies',
@@ -48,10 +41,6 @@ const App = () => {
 
   const fieldName = 'age' 
 
-  object1.address = 'Helsinki'
-  object1['secret number'] = 12341
-  console.log(object1)
-
   const square = p => p * p
   console.log(square(2))
   const tSquared = t.map(p => p * p)
@@ -69,12 +58,65 @@ const App = () => {
   }
   console.log(average(2, 5)) // 3.5
 
+  const arto = {
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
+    greet: function() {
+      console.log('hello, my name is ' + this.name)
+    },
+    doAddition: function(a, b) {
+      console.log(a + b)
+    },
+  }
+  console.log(arto)
+
+  arto.doAddition(1, 4)        // 5 is printed
+  const referenceToAddition = arto.doAddition
+  referenceToAddition(10, 15)   // 25 is printed
+
+  arto.greet()  // "hello, my name is Arto Hellas" gets printed
+  /*
+  const referenceToGreet = arto.greet
+  referenceToGreet() // prints "hello, my name is undefined"
+  */
+ // setTimeout(arto.greet, 1000)
+ setTimeout(arto.greet.bind(arto), 1000)
+
+  arto.growOlder = function() {
+    this.age += 1
+  }
+  
+  console.log(arto.age)   // 35 is printed
+  arto.growOlder()
+  console.log(arto.age)   // 36 is printed
+
+  arto.address = 'Helsinki'
+  arto['secret number'] = 12341
+  console.log(arto)
+
+  class Person {
+    constructor(name, age) {
+      this.name = name
+      this.age = age
+    }
+    greet() {
+      console.log('hello, my name is ' + this.name)
+    }
+  }
+  
+  const adam = new Person('Adam Ondra', 35)
+  adam.greet()
+  
+  const janja = new Person('Janja Garnbret', 22)
+  janja.greet()
+
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
-      <Hello name={object1.name} age={object1[fieldName]} />
+      <Hello name={arto.name} age={arto[fieldName]} />
     </div>
   )
 }
