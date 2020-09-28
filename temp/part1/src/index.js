75969073b3a8081395a7e7e7b2627033700224ca
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-09-28 13:00:08 +0200

    1d Do Not Define Components Within Components [part1]

--- temp/part1/src/index.js
+++ temp/part1/src/index.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Display = props => <div>{props.value}</div>
+
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
   </button>
 )
 
-const App = (props) => {
+const App = props => {
   const [value, setValue] = useState(10)
 
-  const setToValue = (newValue) => {
+  const setToValue = newValue => {
     setValue(newValue)
   }
 
   return (
     <div>
-      {value}
+      <Display value={value} />
       <Button handleClick={() => setToValue(1000)} text="thousand" />
       <Button handleClick={() => setToValue(0)} text="reset" />
       <Button handleClick={() => setToValue(value + 1)} text="increment" />
