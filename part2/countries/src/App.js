ab1996eeb0637ce0b6234e68410204382c14c4fc
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-09-30 12:55:48 +0200

    2.12 step1 start [part2c]

---
+++ part2/countries/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+
+  return (
+    <div>
+      <div>
+        find countries
+        <input />
+      </div>
+      <div>
+        Too many matches, specify another filter
+      </div>
+    </div>
+  )
+}
+
+export default App
