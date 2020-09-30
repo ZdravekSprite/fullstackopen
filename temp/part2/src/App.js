a4bbe3986a657a5f38d1654504d57905d2150b1e
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-09-30 19:58:02 +0200

    Promises and errors [part2d]

--- temp/part2/src/App.js
+++ temp/part2/src/App.js
@@ -3,8 +3,8 @@ import Note from './components/Note'
 import noteService from './services/notes'
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('') 
+  const [notes, setNotes] = useState([]) 
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
@@ -15,25 +15,15 @@ const App = () => {
       })
   }, [])
 
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-
-    noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-      })
-  }
-
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() > 0.5
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
     }
-
+  
     noteService
       .create(noteObject)
       .then(returnedNote => {
@@ -42,8 +32,24 @@ const App = () => {
       })
   }
 
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))     
+      })
+  }
+
   const handleNoteChange = (event) => {
-    //console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
