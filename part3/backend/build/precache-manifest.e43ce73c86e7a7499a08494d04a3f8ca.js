c403488213fb9159db5609c7c2e89b9e7eb877e6
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-06 16:00:53 +0200

    3.11 phonebook full stack end [part3b]

---
+++ part3/backend/build/precache-manifest.e43ce73c86e7a7499a08494d04a3f8ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c961fe55bc236cfbe07d81fced79a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c3619b18279a47ee381",
+    "url": "/static/css/main.064b7281.chunk.css"
+  },
+  {
+    "revision": "dc054b7291c489d4377c",
+    "url": "/static/js/2.fe859063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe859063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3619b18279a47ee381",
+    "url": "/static/js/main.3c1c66e5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
