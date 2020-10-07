58e6e7e43b615a1315b19f30ae31e9412395c1ad
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-07 19:30:15 +0200

    first commit [part4a]

---
+++ temp/part4/notes-backend/build/precache-manifest.d7b67a8c99d07cff9bc40b3c9933cb3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381980d6de8e0a32800615124496019a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a61e1b33e60800a1a1e",
+    "url": "/static/css/main.4187a5f6.chunk.css"
+  },
+  {
+    "revision": "2bca4a2f2c8e576ef16a",
+    "url": "/static/js/2.e38ffbfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e38ffbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a61e1b33e60800a1a1e",
+    "url": "/static/js/main.17ec5e7e.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
