9243e4a4971f91f2294904ad2d05740cac579d59
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-07 14:24:31 +0200

    Promise chaining [part3d]

--- temp/part3/index.js
+++ temp/part3/index.js
@@ -24,10 +24,12 @@ app.post('/api/notes', (request, response, next) => {
     date: new Date(),
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote.toJSON())
-    })
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote)
+    }) 
     .catch(error => next(error))
 })
 
