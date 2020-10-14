3640644943095e031dfef14fd082c9e2dab84c26
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-14 19:16:04 +0200

    Populate [part4c]

--- temp/part4/notes-backend/app.js
+++ temp/part4/notes-backend/app.js
@@ -11,12 +11,12 @@ const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
+    logger.error('error connection to MongoDB:', error.message)
   })
 
 app.use(cors())
@@ -24,8 +24,8 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/notes', notesRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
