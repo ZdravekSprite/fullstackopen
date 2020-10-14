7b97b3de5139636b0b94d289735dc2ba5354c5e9
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-14 15:42:18 +0200

    4.13 Blog list expansions, step1 end [part4b]

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -99,6 +99,28 @@ describe('step5', () => {
       .expect(400)
   })
 })
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+
+    const urls = blogsAtEnd.map(r => r.url)
+
+    expect(urls).not.toContain(blogToDelete.url)
+  })
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
