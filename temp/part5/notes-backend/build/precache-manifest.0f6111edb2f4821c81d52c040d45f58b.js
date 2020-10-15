d6fdea9ec8548dade9afeb7bfeb16708bfcd30e5
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-15 18:54:06 +0200

    notes back 4.9 [part5a]

---
+++ temp/part5/notes-backend/build/precache-manifest.0f6111edb2f4821c81d52c040d45f58b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3558ca067771b65e78761588f87e8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82bc6739826f6d03cac8",
+    "url": "/static/css/main.4187a5f6.chunk.css"
+  },
+  {
+    "revision": "327519329ab96b21beab",
+    "url": "/static/js/2.985ccb7a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.985ccb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82bc6739826f6d03cac8",
+    "url": "/static/js/main.46fe53a6.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
