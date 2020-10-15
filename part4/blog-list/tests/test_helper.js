12e1e5d35ca469f0e327b9c13fb703b35d4bd24f
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-15 09:58:29 +0200

    4.15: bloglist expansion, step3 end [part4d]

--- part4/blog-list/tests/test_helper.js
+++ part4/blog-list/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const oneBlog = {
   title: "Test blog title",
@@ -63,6 +64,11 @@ const blogsInDb = async () => {
   return blogs.map(b => b.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
-  initialBlogs, oneBlog, blogsInDb
+  initialBlogs, oneBlog, blogsInDb, usersInDb
 }
