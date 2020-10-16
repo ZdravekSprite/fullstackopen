d1ed524532bc1a87e649edcb9dbe780b02ee7eaa
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-16 14:41:27 +0200

    5.9* Blog list frontend, step9 end [part5a]

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -116,7 +116,7 @@ const App = () => {
       <Togglable buttonLabel="new blog" ref={blogFormRef}>
         <BlogForm createBlog={addBlog} />
       </Togglable>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
