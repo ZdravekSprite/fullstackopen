ec50be119ece371b354dda46211f4e0dc1bc1dd8
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-18 08:33:11 +0200

    temp 5.10 [part5a]

--- part5/bloglist-frontend/src/components/BlogForm.test.js
+++ part5/bloglist-frontend/src/components/BlogForm.test.js
@@ -10,20 +10,26 @@ test('<BlogForm /> updates parent state and calls onSubmit', () => {
     <BlogForm createBlog={createBlog} />
   )
 
+  const blog = {
+    title: 'Type wars Robert C. Martin',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+  }
+
   const title = component.container.querySelector('#title')
   const author = component.container.querySelector('#author')
   const url = component.container.querySelector('#url')
   const form = component.container.querySelector('form')
 
-  fireEvent.change(title, { target: { value: 'Type wars Robert C. Martin' } })
-  fireEvent.change(author, { target: { value: 'Robert C. Martin' } })
-  fireEvent.change(url, { target: { value: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html' } })
+  fireEvent.change(title, { target: { value: blog.title } })
+  fireEvent.change(author, { target: { value: blog.author } })
+  fireEvent.change(url, { target: { value: blog.url } })
   fireEvent.submit(form)
 
   expect(createBlog.mock.calls).toHaveLength(1)
-  
-  expect(createBlog.mock.calls[0][0].title).toBe('Type wars Robert C. Martin' )
-  expect(createBlog.mock.calls[0][0].author).toBe('Robert C. Martin' )
-  expect(createBlog.mock.calls[0][0].url).toBe('http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html' )
- 
+
+  expect(createBlog.mock.calls[0][0].title).toBe(blog.title)
+  expect(createBlog.mock.calls[0][0].author).toBe(blog.author)
+  expect(createBlog.mock.calls[0][0].url).toBe(blog.url)
+
 })
