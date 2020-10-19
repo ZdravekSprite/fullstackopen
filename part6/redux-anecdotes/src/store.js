97e785e5db950b29d151e65da50163c8f6cd96cd
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-19 15:47:21 +0200

    6.9 Better anecdotes, step7 [part6]

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const store = (reducer) => createStore(
+  reducer,
+  composeWithDevTools()
+)
+
+export default store
