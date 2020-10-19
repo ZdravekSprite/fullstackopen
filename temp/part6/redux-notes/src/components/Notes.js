2e70c1f1421252fabe3612d2c729c4a7cfad2fce
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-19 11:54:05 +0200

    temp 6.1 [part6]

--- temp/part6/redux-notes/src/components/Notes.js
+++ temp/part6/redux-notes/src/components/Notes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { toggleImportanceOf } from './reducers/noteReducer'
+import { toggleImportanceOf } from '../reducers/noteReducer'
 
 const Note = ({ note, handleClick }) => {
   return (
