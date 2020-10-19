a8d54ef22342aa550989a6981fd75be770afa271
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-19 14:14:18 +0200

    Combined reducers [part6]

--- temp/part6/redux-notes/src/App.js
+++ temp/part6/redux-notes/src/App.js
@@ -3,11 +3,22 @@ import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value)
+  }
 
   return (
     <div>
       <NewNote />
-      <Notes  />
+      <div>
+        all          <input type="radio" name="filter"
+          onChange={() => filterSelected('ALL')} />
+        important    <input type="radio" name="filter"
+          onChange={() => filterSelected('IMPORTANT')} />
+        nonimportant <input type="radio" name="filter"
+          onChange={() => filterSelected('NONIMPORTANT')} />
+      </div>
+      <Notes />
     </div>
   )
 }
