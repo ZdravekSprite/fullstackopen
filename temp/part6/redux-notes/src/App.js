bf1da7a4acfc169df1d0259cd3be6c45433fbbf7
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-23 21:03:22 +0200

    Using the connect-function to share the redux store to components [part6]

--- temp/part6/redux-notes/src/App.js
+++ temp/part6/redux-notes/src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import NewNote from './components/NewNote'
-import Notes from './components/Notes'
+import Notes from './components/ConnectedNotes'
 import VisibilityFilter from './components/VisibilityFilter'
 import { initializeNotes } from './reducers/noteReducer'
 import { useDispatch } from 'react-redux'
