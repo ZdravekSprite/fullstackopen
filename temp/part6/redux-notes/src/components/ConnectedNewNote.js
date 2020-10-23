8b190fd280826f157dc84044b7da997cfa0bec14
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-23 21:22:34 +0200

    Referencing action creators passed as props [part6]

--- temp/part6/redux-notes/src/components/ConnectedNewNote.js
+++ temp/part6/redux-notes/src/components/ConnectedNewNote.js
@@ -3,7 +3,9 @@ import { connect } from 'react-redux'
 import { createNote } from '../reducers/noteReducer'
 
 const NewNote = (props) => {
-  
+  console.log(createNote)
+  console.log(props.createNote)
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
