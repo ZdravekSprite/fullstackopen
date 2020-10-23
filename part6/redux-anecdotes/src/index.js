f1362449cca5b0430f68b10e0c123e73e43d22a7
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-23 11:14:08 +0200

    6.15 Anecdotes and the backend, step3 [part6]

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,22 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
 
-import anecdoteReducer from './reducers/anecdoteReducer'
-import notificationReducer from './reducers/notificationReducer'
-import filterReducer from './reducers/filterReducer'
-import store from './store'
-
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  notification: notificationReducer,
-  filter: filterReducer
-})
-
 ReactDOM.render(
-  <Provider store={store(reducer)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,9 +1,22 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = (reducer) => createStore(
+import anecdoteReducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
+import filterReducer from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  notification: notificationReducer,
+  filter: filterReducer
+})
+
+const store = createStore(
   reducer,
-  composeWithDevTools()
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
 )
 
 export default store
