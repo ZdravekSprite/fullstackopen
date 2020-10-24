71ee79d78ad06ee0b6e90515dbae95c512e23162
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-24 14:25:09 +0200

    Custom hooks 1 [part7]

--- temp/part7/counter-app/src/index.js
+++ temp/part7/counter-app/src/index.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const useCounter = () => {
+  const [value, setValue] = useState(0)
+
+  const increase = () => {
+    setValue(value + 1)
+  }
+
+  const decrease = () => {
+    setValue(value - 1)
+  }
+
+  const zero = () => {
+    setValue(0)
+  }
+
+  return {
+    value,
+    increase,
+    decrease,
+    zero
+  }
+}
+
 const App = (props) => {
-  const [counter, setCounter] = useState(0)
+  const counter = useCounter()
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
+      <div>{counter.value}</div>
+      <button onClick={counter.increase}>
         plus
       </button>
-      <button onClick={() => setCounter(counter - 1)}>
+      <button onClick={counter.decrease}>
         minus
       </button>
-      <button onClick={() => setCounter(0)}>
+      <button onClick={counter.zero}>
         zero
       </button>
     </div>
