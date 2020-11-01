110be4f1be800bebf52372de4701940e655f20ee
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-11-01 11:01:14 +0100

    Handling mutation errors [part8]

--- temp/part8/graphql-phonebook-frontend/src/components/PersonForm.js
+++ temp/part8/graphql-phonebook-frontend/src/components/PersonForm.js
@@ -1,23 +1,30 @@
 import React, { useState } from 'react'
 import { useMutation } from '@apollo/client'
+
 import { ALL_PERSONS, CREATE_PERSON } from '../queries'
 
-const PersonForm = () => {
+const PersonForm = ({ setError }) => {
   const [name, setName] = useState('')
   const [phone, setPhone] = useState('')
   const [street, setStreet] = useState('')
   const [city, setCity] = useState('')
 
-  const [ createPerson ] = useMutation(CREATE_PERSON, {
-    refetchQueries: [  {query: ALL_PERSONS} ]
+
+  const [createPerson] = useMutation(CREATE_PERSON, {
+    refetchQueries: [{ query: ALL_PERSONS }],
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message)
+    }
   })
 
-  const submit = (event) => {
+  const submit = async (event) => {
     event.preventDefault()
 
-    createPerson({ variables: { name, phone, street, city } })
+    createPerson({
+      variables: { name, phone, street, city }
+    })
 
-    setName('')
+    setName('Arto Hellas')
     setPhone('')
     setStreet('')
     setCity('')
@@ -28,22 +35,26 @@ const PersonForm = () => {
       <h2>create new</h2>
       <form onSubmit={submit}>
         <div>
-          name <input value={name}
+          name <input
+            value={name}
             onChange={({ target }) => setName(target.value)}
           />
         </div>
         <div>
-          phone <input value={phone}
+          phone <input
+            value={phone}
             onChange={({ target }) => setPhone(target.value)}
           />
         </div>
         <div>
-          street <input value={street}
+          street <input
+            value={street}
             onChange={({ target }) => setStreet(target.value)}
           />
         </div>
         <div>
-          city <input value={city}
+          city <input
+            value={city}
             onChange={({ target }) => setCity(target.value)}
           />
         </div>
