6bc77cf061d80eb8d7b33dc5f6efcdd8603b93b2
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-11-09 17:38:53 +0100

    switch...case [part9]

--- temp/part9/typescript/multiplier.ts
+++ temp/part9/typescript/multiplier.ts
@@ -1,21 +1,29 @@
-type Operation = 'multiply' | 'add' | 'divide';
-
-type Result = string | number
-
-const calculator = (a: number, b: number, op: Operation): Result =>  {
-
-  if (op === 'multiply') {
-    return a * b;
-  } else if (op === 'add') {
-    return a + b;
-  } else if (op === 'divide') {
-    if (b === 0) return 'this cannot be done';
-    return a / b;
-  }
-}
-
 const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText, a * b);
 }
 
 multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
+
+type Operation = 'multiply' | 'add' | 'divide';
+
+type Result = number;
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+  switch (op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add or divide!');
+  }
+}
+
+try {
+  console.log(calculator(1, 5, 'divide'))
+} catch (e) {
+  console.log('Something went wrong, error message: ', e.message);
+}
