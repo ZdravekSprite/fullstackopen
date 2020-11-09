957e720b5d82115f38184a52910ad3d84752aaac
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-11-09 17:35:32 +0100

    Creating your first own types [part9]

--- temp/part9/typescript/multiplier.ts
+++ temp/part9/typescript/multiplier.ts
@@ -1,4 +1,20 @@
-const multiplicator = (a, b, printText) => {
+type Operation = 'multiply' | 'add' | 'divide';
+
+type Result = string | number
+
+const calculator = (a: number, b: number, op: Operation): Result =>  {
+
+  if (op === 'multiply') {
+    return a * b;
+  } else if (op === 'add') {
+    return a + b;
+  } else if (op === 'divide') {
+    if (b === 0) return 'this cannot be done';
+    return a / b;
+  }
+}
+
+const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText, a * b);
 }
 
