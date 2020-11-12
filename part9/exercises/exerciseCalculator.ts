a8c27c15f9531322aa7e0774c01b857597131d48
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-11-12 19:08:54 +0100

    9.3 Command line [part9]

--- part9/exercises/exerciseCalculator.ts
+++ part9/exercises/exerciseCalculator.ts
@@ -1,3 +1,29 @@
+interface ExerciseMultiplyValues {
+  value1: number[];
+  value2: number;
+}
+
+const exerciseParseArguments = (args: Array<string>): ExerciseMultiplyValues => {
+  let value1 = [];
+  let value2;
+  if (args.length < 4) throw new Error('Not enough arguments');
+  for (var i = 2; i < args.length; i++) {
+    if (!isNaN(Number(args[i]))) {
+      if (i === 2) {
+        value2 = Number(args[i]);
+      } else {
+        value1.push(Number(args[i]));
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+  }
+  return {
+    value1: value1,
+    value2: value2
+  }
+}
+
 interface ExerciseResult {
   periodLength: number;
   trainingDays: number;
@@ -24,7 +50,7 @@ const calculateExercises = (exerciseHours: number[], target: number): ExerciseRe
   if (average >= target) {
     ratingDescription = 'great';
     rating = 3;
-  } else if (average > 0.9 * target) {
+  } else if (average > 0.85 * target) {
     ratingDescription = 'not too bad but could be better';
     rating = 2;
   } else {
@@ -43,4 +69,12 @@ const calculateExercises = (exerciseHours: number[], target: number): ExerciseRe
   };
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+//console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+
+try {
+  const { value1, value2 } = exerciseParseArguments(process.argv);
+  //calculateExercises(value1, value2);
+  console.log(calculateExercises(value1, value2));
+} catch (e) {
+  console.log('Error, something bad happened, message: ', e.message);
+}
