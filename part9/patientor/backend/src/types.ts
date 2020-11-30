4891a6c8881b456741ebed391c93b37237a7299f
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-11-30 12:39:28 +0100

    9.11: Patientor backend, step4 [part9]

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -5,3 +5,16 @@ export interface DiagnoseEntry {
 }
 
 //export type Diagnose = 'ok' | 'not ok';
+
+export type Gender = 'male' | 'female' | 'other';
+
+export interface PatientEntry {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: Gender;
+  occupation: string;
+}
+
+export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
