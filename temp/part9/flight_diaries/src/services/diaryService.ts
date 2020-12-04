eb748daee8dfb10ec6dceae13868f963f7093917
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-12-04 08:18:59 +0100

    Adding a new diary [part9]

--- temp/part9/flight_diaries/src/services/diaryService.ts
+++ temp/part9/flight_diaries/src/services/diaryService.ts
@@ -1,11 +1,17 @@
 import diaries from '../../data/diaries';
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+import {
+  NewDiaryEntry,
+  NonSensitiveDiaryEntry,
+  DiaryEntry,
+  Visibility,
+  Weather
+} from '../types';
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
 };
 
-const getNonSensitiveEntries = (): NonSensitiveDiaryEntry [] => {
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   return diaries.map(({ id, date, weather, visibility }) => ({
     id,
     date,
@@ -14,8 +20,20 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry [] => {
   }));
 };
 
-const addEntry = () => {
-  return null;
+const addEntry = (
+  date: string, weather: Weather, visibility: Visibility, comment: string
+): DiaryEntry => {
+
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    date,
+    weather,
+    visibility,
+    comment,
+  }
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 const findById = (id: number): DiaryEntry | undefined => {
@@ -23,9 +41,20 @@ const findById = (id: number): DiaryEntry | undefined => {
   return entry;
 }
 
+const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
+};
+
 export default {
   getEntries,
   addEntry,
   getNonSensitiveEntries,
-  findById
+  findById,
+  addDiary
 };
