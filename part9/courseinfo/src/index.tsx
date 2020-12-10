bae084efd94b1a5627f3a144bf7c8a7c1f3b0dea
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-12-10 11:55:01 +0100

    Exercise 9.15. [part9]

--- part9/courseinfo/src/index.tsx
+++ part9/courseinfo/src/index.tsx
@@ -3,18 +3,47 @@ import ReactDOM from "react-dom";
 
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  // new types
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+  }
+
+  interface CoursePartOne extends CoursePartBase {
+    name: "Fundamentals";
+    description: string;
+  }
+
+  interface CoursePartTwo extends CoursePartBase {
+    name: "Using props to pass data";
+    groupProjectCount: number;
+  }
+
+  interface CoursePartThree extends CoursePartBase {
+    name: "Deeper type usage";
+    description: string;
+    exerciseSubmissionLink: string;
+  }
+
+  type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree;
+
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
     }
   ];
 
